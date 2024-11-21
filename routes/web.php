74d1498ec9423eca836a061c372529da211d882a
ARIFSATRIA1 <arifsatria5554@gmail.com>
2024-11-21 17:18:52 +0700

    feat: add admin controller

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GuestController;
-// use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', [GuestController::class, 'index'])->name('home');
 Route::get('/login', [GuestController::class, 'login'])->name('login');
@@ -14,6 +15,8 @@ Route::post('/postregister', [AuthController::class, 'postRegister']);
 Route::post('postlogin', [AuthController::class, 'postLogin']);
 
 
-
+Route::middleware(['admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index']);
+});
 
 
