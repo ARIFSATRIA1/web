0380b24a3252a5d46e3d8bd6ee8c33ae91d25e03
ARIFSATRIA1 <arifsatria5554@gmail.com>
2024-11-22 14:05:51 +0700

    fix: bug

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,5 +1,176 @@
-<header>
-    <nav>
+@extends('home.templates.header')
+@section('content')
 
-    </nav>
-</header>
+    <section>
+        @include('home.templates.navbar')
+    </section>
+
+    <main>
+        <section id="home">
+            <div class="d-flex align-items-center justify-content-center flex-column">
+                <img src="/img/Banner.png" class="img-fluid" style="min-height: 15rem" alt="">    
+            </div>
+        </section>      
+
+        <section id="about">
+            <div class="container-fluid mt-5">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-4 d-flex justify-content-end">
+                        <img src="/img/group2.png" class="img-fluid rounded-3" alt="">
+                    </div>
+                    <div class="col-md-6 ">
+                        <h1 class="text-secondary">Tentang Kami</h1>
+                        <img src="/img/main-content.png" style="min-width: 95%" class="img-fluid rounded-3" alt="">
+                        <p class="background-gradient-yellow py-3 mt-3 rounded-3 px-3 mr-5 text-left fs-5" style="color:blue; position: relative; display: block; max-width:95%">
+                            Program yang dibentuk oleh DB Klik, Perusahaan IT Indonesia. Kami <br>
+                            berkomitmen untuk berkontribusi dalam dunia pendidikan di Indonesia <br>
+                            <br>
+                            Kami memberikan fasilitas seperti program, kelas, dan forum yang <br>
+                            ditawarkan agar generasi muda Indonesia memiliki bekal dengan <br>
+                            keterampilan yang relevan di dunia industry saat ini <br>
+                        </p>                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="dbAcademyOffer" class="background-content mt-5" style="position: relative; display:inherit">
+            <h1 class="text-center text-white pt-5">DB Academy <span class="text-warning">Tawarkan</span></h1>
+                <div class="container pt-5 pb-5">
+                    <div class="row d-flex align-items-center">
+                
+                        <div class="col-6 col-sm-6 col-md-6 mb-2 d-flex align-items-center justify-content-center">
+                            <div class="d-flex flex-row bg-transparent" style="border: none;">
+                                <img src="/img/icon_intern.png" class="rounded-circle " style="width:6rem; height:6rem;" alt="...">
+                                <div class="text-white">
+                                    <h5 class="bg-light text-primary w-100 rounded-2 text-center mx-2 px-2" style="display: inline;">Internship Program</h5>
+                                    <p class="mx-2">
+                                        Program magang dengan real <br>
+                                         project <br>
+                                        yang bisa mengasah
+                                    </p>
+                                    <p class="text-warning" style="font-size: 0.8em">Soft Skill,Hard Skill & Support Skill</p>
+                                </div>
+                            </div>
+                        </div>
+                  
+                        <div class="col-6 col-sm-6 col-md-6 mb-2 d-flex align-items-center justify-content-center">
+                            <div class="d-flex flex-row bg-transparent" style="border: none;">
+                                <img src="/img/icon.group.png" class="rounded-circle" style="width:6rem; height:6rem;" alt="...">
+                                <div class="text-white">
+                                    <h5 class="bg-light text-primary w-100 rounded-2 text-center mx-2 pe-5 ps-2" style="display: inline;">Group Project</h5>
+                                    <p class="mx-2">
+                                        Internship group project berbagai role <br>
+                                        position bisa dipilih melalui program </p>
+                                    </p>
+                                    <p class="text-warning" style="font-size: 0.8em">#DreamBig Group Project</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-6 col-sm-6 col-md-6 d-flex align-items-center justify-content-center">
+                            <div class="d-flex flex-row bg-transparent" style="border: none;">
+                                <img src="/img/icon_bootcamp.png" class="rounded-circle" style="width:6rem; height:6rem;" alt="...">
+                                <div class="text-white">
+                                    <h5 class="bg-light text-primary w-100 rounded-2 text-center mx-2 pe-4 ps-2" style="display: inline;">Kelas Coding</h5>
+                                    <p class="mx-2 pe-1">
+                                        Program coding yang dibentuk<br>
+                                        untuk anak usia dini melalui <br>
+                                        program DB EDU <br>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-6 col-sm-6 col-md-6  d-flex align-items-center justify-content-center">
+                            <div class="d-flex flex-row bg-transparent" style="border: none;">
+                                <img src="/img/icon_kelas.png" class="rounded-circle" style="width:6rem; height:6rem;" alt="...">
+                                <div class="text-white">
+                                    <h5 class="bg-light text-primary w-100 rounded-2 text-center mx-2 pe-5 ps-2" style="display: inline;">Bootcamp</h5>
+                                    <p class="mx-2">
+                                        Program kelas online dengan mentor <br>
+                                        dari DB Academy dan luar yang <br>
+                                        sudah expert di bidangnya
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>   
+        </section>
+
+        <section id="blog">
+            <div class="container d-flex flex-column align-items-center mt-5">
+                <div class="background-container rounded-5  d-flex align-items-center mb-5">
+                    <h3 class="py-2 pr-3 text-center">
+                        <span class="background-content p-5 rounded-circle text-warning">Skill</span>
+                        <span class="text-primary ps-2 pe-4 text-black">yang didapat</span>
+                    </h3>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    @foreach([
+                        'dm' => 'DIGITAL MARKETING', 
+                        'cc' => 'CONTENT CREATOR', 
+                        'prog' => 'PROGRAMMING',
+                        'ui' => 'UI UX DESIGN', 
+                        'edtcontent' => 'EDITING CONTENT', 
+                        'journalist' => 'JOURNALIST & COPYWRITING', 
+                        'leadership' => 'LEADERSHIP', 
+                        'teamwork' => 'TEAMWORK',
+                        'strategic' => 'STRATEGIC THINKING', 
+                        'communication' => 'COMMUNICATION'] as $key => $skill)
+                        <div class="col-6 col-md-2 mb-3">
+                            <div class="card d-flex flex-column img-fluid pb-2 background-card-{{ $key }}" style="min-height: 16rem; background-size: cover; background-position: center;">
+                                <h4 class="card-title text-center mt-auto mb-1 text-warning">{{ $skill }}</h4>
+                                <p class="card-text bg-white text-center rounded-5 mx-auto" style="color: black; display: inline-block; padding: 0.1rem 1rem;">Skill</p>
+                            </div>
+                        </div>
+
+                        @if (($loop->iteration % 5=== 0) && !$loop->last)
+                            <div class="w-100 d-none d-md-block"></div>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+        </section>
+
+
+        <section id="contact" class="my-5">
+            <div class="container">
+                <div class="row d-flex justify-content-center align-items-center">
+                    <div class="col-md-5">
+                        <img src="/img/Bintang.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-7 align-items-center">
+                        <div class="d-flex flex-column"></div>
+                        <p class="background-gradient-yellow py-3 mt-3 rounded-3 px-3 mr-5 text-left fs-5" style="color:blue; position: relative; display: flex; max-width:95%"> 
+                            <span>Koneksi Indsutri dengan bekerja  <br>
+                                sama dengan stakeholder terkait<br></span>
+                        </p> 
+                        <p class="background-gradient-yellow py-3 mt-3 rounded-3 px-3 mr-5 text-left fs-5" style="color:blue; position: relative; display: flex; max-width:95%">
+                            Fokus pada praktik dengan belajar <br>
+                            sambil melakukan proyek nyata<br>
+                        </p> 
+                        <p class="background-gradient-yellow py-3 mt-3 rounded-3 px-3 mr-5 text-left fs-5" style="color:blue; position: relative; display: flex; max-width:95%">
+                            Materi selalu diperbarui mengikuti <br>
+                            perkembangan teknologi terkini <br>
+                        </p> 
+                        <p class="background-gradient-yellow py-3 mt-3 rounded-3 px-3 mr-5 text-left fs-5" style="color:blue; position: relative; display: flex; max-width:95%">
+                            Real program untuk mengasah soft <br>
+                            skill, hard skill dan support skill <br>
+                        </p> 
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
+
+    <section class="mt-5">
+        @include('home.templates.footer')
+    </section>
+
+    
+@endsection
---
+++ resources/views/home/templates/header.blade.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>@yield('title')</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Smooch&display=swap" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+    <style>
+        .roboto-thin {
+            font-family: "Roboto", sans-serif;
+            font-weight: 100;
+            font-style: normal;
+        }
+
+        .roboto-light {
+            font-family: "Roboto", sans-serif;
+            font-weight: 300;
+            font-style: normal;
+        }
+
+        .roboto-regular {
+            font-family: "Roboto", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+        }
+
+        .roboto-medium {
+            font-family: "Roboto", sans-serif;
+            font-weight: 500;
+            font-style: normal;
+        }
+
+        .roboto-bold {
+            font-family: "Roboto", sans-serif;
+            font-weight: 700;
+            font-style: normal;
+        }
+
+        .roboto-black {
+            font-family: "Roboto", sans-serif;
+            font-weight: 900;
+            font-style: normal;
+        }
+
+        .roboto-thin-italic {
+            font-family: "Roboto", sans-serif;
+            font-weight: 100;
+            font-style: italic;
+        }
+
+        .roboto-light-italic {
+            font-family: "Roboto", sans-serif;
+            font-weight: 300;
+            font-style: italic;
+        }
+
+        .roboto-regular-italic {
+            font-family: "Roboto", sans-serif;
+            font-weight: 400;
+            font-style: italic;
+        }
+
+        .roboto-medium-italic {
+            font-family: "Roboto", sans-serif;
+            font-weight: 500;
+            font-style: italic;
+        }
+
+        .roboto-bold-italic {
+            font-family: "Roboto", sans-serif;
+            font-weight: 700;
+            font-style: italic;
+        }
+
+        .roboto-black-italic {
+            font-family: "Roboto", sans-serif;
+            font-weight: 900;
+            font-style: italic;
+        }
+
+        .smooch-regular {
+            font-family: "Smooch", cursive;
+            font-weight: 400;
+            font-style: normal;
+        }
+
+        .sectionstyles {
+            background-image: url("/img/bg-main.png");
+            position: relative;
+            color: #ffff;
+        }
+
+        .introduction {
+            font-family: "Great Vibes";
+            margin-top: 10rem;
+
+        }
+
+        @media (max-width: 450px) {
+            html {
+                font-size: 55%;
+            }
+
+            
+            }
+
+        ::placeholder {
+            color: white !important;
+            opacity: 1;
+        }
+        .sotk-text {
+            font-size: 24px !important;
+            color: #424242;
+            margin: 0 280px 0 280px !important;
+        }
+
+        .skill-highlight {
+            background: linear-gradient(160deg, rgba(37,133,243,1) 35%, rgba(1,19,165,1) 100%);
+            color: #ffd700;
+            padding: 0.5rem 1rem;
+            font-weight: bold;
+        }
+
+        .background-span {
+            color: #0055ff;
+            font-weight: bold;
+        }
+
+        .background-container {
+            display: inline-flex;
+            align-items: center;
+            overflow: hidden; 
+            background: linear-gradient(160deg, rgba(249,232,28,1) 35%, rgba(243,185,23,1) 100%);
+            position: relative;
+             display: inline-flex;
+        }
+
+        .background-content {
+            background: linear-gradient(160deg, rgba(37,133,243,1) 35%, rgba(1,19,165,1) 100%);
+        }
+
+        .background-gradient-yellow {
+            background: linear-gradient(160deg, rgba(249,232,28,1) 35%, rgba(243,185,23,1) 100%);
+        }
+        .background-card-dm {
+            background-image: 
+            linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/digital_mkt.png');
+        }
+
+        .background-card-dm:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+
+        .background-card-cc {
+            background-image:
+                linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/content_creator.png');
+        }
+
+        .background-card-cc:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+
+        .background-card-prog {
+            background-image:
+                linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/programming.png');
+        }
+
+        .background-card-prog:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+
+        .background-card-ui {
+            background-image:
+                linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/ui-ux.png');
+        }
+
+        .background-card-ui:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+
+        .background-card-edtcontent {
+            background-image:
+                linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/editing_content.png');
+        }
+
+        .background-card-edtcontent:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+
+        .background-card-journalist {
+            background-image:
+                linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/journalist.png');
+        }
+
+        .background-card-journalist:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+
+        .background-card-leadership {
+            background-image:
+                linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/leadership.png');
+        }
+
+        .background-card-leadership:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+
+        .background-card-teamwork {
+            background-image:
+                linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/teamwork.png');
+        }
+
+        .background-card-teamwork:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+
+        .background-card-strategic {
+            background-image:
+                linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/strategic_thinking.png');
+        }
+
+        .background-card-strategic:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+
+        .background-card-communication {
+            background-image:
+                linear-gradient(180deg, rgba(37,133,243,0) 0%, rgba(33,119,234,0.020045518207282953) 6%, rgba(6,36,177,0.4682247899159664) 84%, rgba(1,19,165,1) 100%), 
+                url('/img/communication.png');
+        }
+        .background-card-communication:hover {
+            transform: scale(1.05);
+            transition: transform 0.3s ease;
+        }
+        
+        .navbar-nav.active {
+            display: inline-block;
+            justify-content: center;
+            align-items: center
+        }   
+    
+
+    </style>
+</head>
+
+<body class="roboto-regular fw-semibold">
+    @yield('content')
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-oBqDVmMz4fnFO9gybNSeRl2R7q/8P5w5NH8g/t8WjTBTk07izN5WgJ5nB3lXwW5a" crossorigin="anonymous"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const navbarNav = document.querySelector("#navbarNav");
+            const hamburger = document.querySelector("#hamburger-menu");
+
+            hamburger.addEventListener("click", function () {
+                navbarNav.classList.toggle("show");
+            });
+        
+        });
+        
+        document.addEventListener("click", function (e) {
+                if (!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+                    navbarNav.classList.remove("active");
+            });
+    </script>
+<footer>
+@yield('footer')
+</footer>
+
+</html>
